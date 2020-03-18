ef6692a2b457b5f773667b56263c9833eda13f0c
acimanx <acimanx@gmail.com>
2020-03-18 10:28:27 +0100

    Updates

---
+++ precache-manifest.c73205ad3354699192459ef1f0e4fc15.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a38ee4daaaa1e8134eddcc9c3a63d1",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "6c3fbb2e1a4c4d7717fb",
+    "url": "/corona/static/css/2.cd50a2d2.chunk.css"
+  },
+  {
+    "revision": "31e3b61ebba40e614c2b",
+    "url": "/corona/static/css/main.8168bc7d.chunk.css"
+  },
+  {
+    "revision": "6c3fbb2e1a4c4d7717fb",
+    "url": "/corona/static/js/2.eaefb8d4.chunk.js"
+  },
+  {
+    "revision": "a310eebfa47e197c9bcd80585a866ff1",
+    "url": "/corona/static/js/2.eaefb8d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e3b61ebba40e614c2b",
+    "url": "/corona/static/js/main.8c680a93.chunk.js"
+  },
+  {
+    "revision": "5503839ed458016943ba",
+    "url": "/corona/static/js/runtime-main.49908466.js"
+  },
+  {
+    "revision": "ed720987806b1885307241d178685516",
+    "url": "/corona/static/media/daily.ed720987.csv"
+  },
+  {
+    "revision": "6365c10eee65250aa833bdad13d80cbf",
+    "url": "/corona/static/media/table.6365c10e.csv"
+  }
+]);
